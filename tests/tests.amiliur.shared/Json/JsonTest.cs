eeeefa7a16dfa6f9b7e580e7033513d05edc9dde
Rui Lima <3x0dv5@users.noreply.github.com>
2023-07-26 23:56:31 +0100

    New features (#18)
    * Fix old namespace
    
    * new featurs
    
    * datetime utils
    
    * udpats
    
    * Return always the result if it's a saveresult since it has the state
    
    * Add new element
    
    * new features
    
    * revert to preview 4
    
    * revert back to dotnet 7
    
    * revert to dotnet 7
    
    * revert to dotnet 7
    
    * revert to dotnet 7

--- tests/tests.amiliur.shared/Json/JsonTest.cs
+++ tests/tests.amiliur.shared/Json/JsonTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using amiliur.shared.Json;
+﻿using amiliur.shared.Json;
 using Xunit.Abstractions;
 
 namespace tests.amiliur.shared.Json;
@@ -20,88 +19,88 @@ public class JsonTest
         {
           "__objectType__": "amiliur.figforms.shared.Models.FormDataSearchInputModel, amiliur.figforms.shared",
           "FormDefinition": {
-            "__objectType__": "amiliur.figforms.shared.FormDefinition, amiliur.figforms.shared",
-            "FormContext": "admin",
-            "FormModule": "saude",
-            "FormCode": "doencas",
-            "FormName": "Doenças",
-            "FormTitle": "Editar Doença",
-            "FormMode": "Edit",
-            "FormDescription": "",
-            "Rows": [
-              {
-                "__objectType__": "amiliur.figforms.shared.FormDefinitionRow, amiliur.figforms.shared",
-                "Cells": [
-                  {
-                    "__objectType__": "amiliur.figforms.shared.FormDefinitionCell, amiliur.figforms.shared",
-                    "Fields": [
-                      {
-                        "__objectType__": "amiliur.figforms.shared.Fields.Models.TextFormFieldModel, amiliur.figforms.shared",
-                        "Multiline": false,
-                        "ReadOnly": true,
-                        "ValidationRules": [],
-                        "FieldBindings": [],
-                        "FieldName": "Code",
-                        "DisplayName": "Código",
-                        "Description": "Código da doença",
-                        "FieldRenderer": {
-                          "__objectType__": "amiliur.figforms.shared.Fields.Models.Renderers.TextFieldRenderer, amiliur.figforms.shared"
-                        },
-                        "IsHidden": false
-                      }
-                    ],
-                    "ColumnBootstrapSpan": 12,
-                    "Visible": true
-                  }
-                ]
-              },
-              {
-                "__objectType__": "amiliur.figforms.shared.FormDefinitionRow, amiliur.figforms.shared",
-                "Cells": [
-                  {
-                    "__objectType__": "amiliur.figforms.shared.FormDefinitionCell, amiliur.figforms.shared",
-                    "Fields": [
-                      {
-                        "__objectType__": "amiliur.figforms.shared.Fields.Models.TextFormFieldModel, amiliur.figforms.shared",
-                        "Multiline": false,
-                        "ReadOnly": false,
-                        "ValidationRules": [
-                          {
-                            "__objectType__": "amiliur.figforms.shared.Validation.RequiredFormFieldValidation, amiliur.figforms.shared",
-                            "AllowEmptyStrings": false,
-                            "ErrorMessageString": "The field `Nome` is required"
-                          }
-                        ],
-                        "FieldBindings": [],
-                        "FieldName": "Nome",
-                        "DisplayName": "Nome",
-                        "Description": "Nome da doença",
-                        "FieldRenderer": {
-                          "__objectType__": "amiliur.figforms.shared.Fields.Models.Renderers.TextFieldRenderer, amiliur.figforms.shared"
-                        },
-                        "IsHidden": false
-                      }
-                    ],
-                    "ColumnBootstrapSpan": 12,
-                    "Visible": true
-                  }
-                ]
-              }
-            ],
-            "SaveOnClick": true,
-            "DataTypeName": "Agroori.Shared.Doencas.DoencaEditModel, Agroori.Shared",
-            "LoadDataSource": {
-              "__objectType__": "amiliur.figforms.shared.ApiDataSource, amiliur.figforms.shared",
-              "ApiUrl": "api/doenca",
-              "ApiMethod": "get-data"
-            }
-          },
+                "__objectType__": "amiliur.figforms.shared.FormDefinition, amiliur.figforms.shared",
+                "FormContext": "admin",
+                "FormModule": "saude",
+                "FormCode": "doencas",
+                "FormName": "Doenças",
+                "FormTitle": "Editar Doença",
+                "FormMode": "Edit",
+                "FormDescription": "",
+                "Rows": [
+                    {
+                        "__objectType__": "amiliur.figforms.shared.FormDefinitionRow, amiliur.figforms.shared",
+                        "Cells": [
+                            {
+                                "__objectType__": "amiliur.figforms.shared.FormDefinitionCell, amiliur.figforms.shared",
+                                "Fields": [
+                                    {
+                                        "__objectType__": "amiliur.figforms.shared.Fields.Models.TextFormFieldModel, amiliur.figforms.shared",
+                                        "Multiline": false,
+                                        "ReadOnly": true,
+                                        "ValidationRules": [],
+                                        "FieldBindings": [],
+                                        "FieldName": "Code",
+                                        "DisplayName": "Código",
+                                        "Description": "Código da doença",
+                                        "FieldRenderer": {
+                                            "__objectType__": "amiliur.figforms.shared.Fields.Models.Renderers.TextFieldRenderer, amiliur.figforms.shared"
+                                        },
+                                        "IsHidden": false
+                                    }
+                                ],
+                                "ColumnBootstrapSpan": 12,
+                                "Visible": true
+                            }
+                        ]
+                    },
+                    {
+                        "__objectType__": "amiliur.figforms.shared.FormDefinitionRow, amiliur.figforms.shared",
+                        "Cells": [
+                            {
+                                "__objectType__": "amiliur.figforms.shared.FormDefinitionCell, amiliur.figforms.shared",
+                                "Fields": [
+                                    {
+                                        "__objectType__": "amiliur.figforms.shared.Fields.Models.TextFormFieldModel, amiliur.figforms.shared",
+                                        "Multiline": false,
+                                        "ReadOnly": false,
+                                        "ValidationRules": [
+                                            {
+                                                "__objectType__": "amiliur.figforms.shared.Validation.RequiredFormFieldValidation, amiliur.figforms.shared",
+                                                "AllowEmptyStrings": false,
+                                                "ErrorMessageString": "The field `Nome` is required"
+                                            }
+                                        ],
+                                        "FieldBindings": [],
+                                        "FieldName": "Nome",
+                                        "DisplayName": "Nome",
+                                        "Description": "Nome da doença",
+                                        "FieldRenderer": {
+                                            "__objectType__": "amiliur.figforms.shared.Fields.Models.Renderers.TextFieldRenderer, amiliur.figforms.shared"
+                                        },
+                                        "IsHidden": false
+                                    }
+                                ],
+                                "ColumnBootstrapSpan": 12,
+                                "Visible": true
+                            }
+                        ]
+                    }
+                ],
+                "SaveOnClick": true,
+                "DataTypeName": "Agroori.Shared.Doencas.DoencaEditModel, Agroori.Shared",
+                "LoadDataSource": {
+                    "__objectType__": "amiliur.figforms.shared.ApiDataSource, amiliur.figforms.shared",
+                    "ApiUrl": "api/doenca",
+                    "ApiMethod": "get-data"
+                }
+            },
           "DataFilter": {
-            "__objectType__": "FieldValueExpr, amiliur.figforms.shared",
-            "Field": "Id",
-            "Value": "80787aa5-5a4a-42de-80a7-4f29847ea9ad",
-            "FilterType": "None"
-          },
+                "__objectType__": "FieldValueExpr, amiliur.figforms.shared",
+                "Field": "Id",
+                "Value": "80787aa5-5a4a-42de-80a7-4f29847ea9ad",
+                "FilterType": "None"
+            },
           "Ts": "2023-05-16T17:46:37.0000000Z",
           "AsOf": "2023-05-16"
         }
